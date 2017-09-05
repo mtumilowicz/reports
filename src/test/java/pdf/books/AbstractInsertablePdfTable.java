2a777e8ab5763ecc3ea8627a6f46ed958351054d
mtumilowicz <michal.tumilowicz01@gmail.com>
2017-09-05 23:04:41 +0200

    refactoring

--- src/test/java/pdf/books/AbstractInsertablePdfTable.java
+++ src/test/java/pdf/books/AbstractInsertablePdfTable.java
@@ -1,5 +1,6 @@
 package pdf.books;
 
+import com.itextpdf.layout.Document;
 import core.bundle.BundleHandler;
 import core.pdf.builder.PdfCellBuilder;
 import core.pdf.writer.AbstractDocumentWriter;
@@ -7,7 +8,7 @@ import core.pdf.writer.AbstractDocumentWriter;
 /**
  * Created by mtumilowicz on 2017-09-05.
  */
-public abstract class AbstractInsertablePdfTable implements InsertablePdfTable {
+public abstract class AbstractInsertablePdfTable {
 
     private final PdfCellBuilder cellBuilder = new PdfCellBuilder();
     private final BundleHandler bundles;
@@ -15,6 +16,8 @@ public abstract class AbstractInsertablePdfTable implements InsertablePdfTable {
     public AbstractInsertablePdfTable(AbstractDocumentWriter writer) {
         this.bundles = writer.getBundles();
     }
+    
+    abstract void insertInto(Document document);
 
     public PdfCellBuilder getCellBuilder() {
         return cellBuilder;
