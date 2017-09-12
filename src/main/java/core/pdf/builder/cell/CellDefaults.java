bc6eca6296d14a542f06900c70d8205b313586fa
mtumilowicz <michal.tumilowicz01@gmail.com>
2017-09-12 18:12:17 +0200

    refactoring

---
+++ src/main/java/core/pdf/builder/cell/CellDefaults.java
@@ -0,0 +1,16 @@
+package core.pdf.builder.cell;
+
+/**
+ * Created by mtumilowicz on 2017-09-12.
+ */
+final class CellDefaults {
+    private int defaultFontSize = 14;
+
+    int getDefaultFontSize() {
+        return defaultFontSize;
+    }
+
+    void setDefaultFontSize(int defaultFontSize) {
+        this.defaultFontSize = defaultFontSize;
+    }
+}
