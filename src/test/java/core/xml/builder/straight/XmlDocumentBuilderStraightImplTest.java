a76272b6c3f0d4961ad3305f0e5d3e40baf9b15f
mtumilowicz <michal.tumilowicz01@gmail.com>
2017-11-08 18:18:44 +0100

    naming improvement

---
+++ src/test/java/core/xml/builder/straight/XmlDocumentBuilderStraightImplTest.java
@@ -0,0 +1,40 @@
+package core.xml.builder.straight;
+
+import core.dom.ControlXmlDocument;
+import core.xml.builder.XmlDocumentBuilderStraightImpl;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
+import static org.custommonkey.xmlunit.XMLAssert.assertXMLNotEqual;
+
+/**
+ * Created by mtumilowicz on 2017-06-15.
+ */
+public class XmlDocumentBuilderStraightImplTest {
+
+    @Before
+    public void setUp() {
+        XMLUnit.setNormalizeWhitespace(true);
+    }
+    
+    @Test
+    public void xmlDocumentBuilderStraightImplConstructor_name() {
+        XmlDocumentBuilderStraightImpl xmlDocumentBuilder = new XmlDocumentBuilderStraightImpl("test");
+        assertXMLEqual(ControlXmlDocument.byPath("src/test/resources/onlyRoot.xml"),
+                xmlDocumentBuilder.getDocument());
+    }
+
+    @Test
+    public void xmlDocumentBuilderChainImplFullMethodsTestPositive() {
+        assertXMLEqual(ControlXmlDocument.byPath("src/test/resources/extendedXmlDoc.xml"),
+                new StraightReportTypeXmlWriterShowcase("node0").prepare());
+    }
+
+    @Test
+    public void xmlDocumentBuilderChainImplFullMethodsTestNegative() {
+        assertXMLNotEqual(ControlXmlDocument.byPath("src/test/resources/extendedXmlDocWrongNode.xml"),
+                new StraightReportTypeXmlWriterShowcase("node0").prepare());
+    }
+}
