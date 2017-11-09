92998238ccd07a9b44b144386b1a51e7f6360d13
mtumilowicz <michal.tumilowicz01@gmail.com>
2017-11-09 13:49:52 +0100

    xlsxCellBuilder sketch

---
+++ src/main/java/core/xlsx/builder/cell/CellText.java
@@ -0,0 +1,56 @@
+package core.xlsx.builder.cell;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.util.CellUtil;
+
+/**
+ * Created by mtumilowicz on 2017-11-09.
+ */
+final class CellText {
+    private String value;
+    private HorizontalAlignment alignment;
+    private int singleCellFontSize;
+    private int setDefaultFontSize;
+
+    void value(String value) {
+        this.value = value;
+    }
+
+    void alignment(HorizontalAlignment alignment) {
+        this.alignment = alignment;
+    }
+
+    void singleCellFontSize(int singleCellFontSize) {
+        this.singleCellFontSize = singleCellFontSize;
+    }
+
+    void setDefaultFontSize(int setDefaultFontSize) {
+        this.setDefaultFontSize = setDefaultFontSize;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    void prepareText(Cell cell) {
+        prepareAlignment(cell);
+
+        singleCellFontSize(cell);
+    }
+
+    private void prepareAlignment(Cell cell) {
+        if (alignment != null) {
+            CellUtil.setAlignment(cell, alignment);
+        }
+    }
+
+    private void singleCellFontSize(Cell cell) {
+        if (singleCellFontSize > 0) {
+            Font font = cell.getSheet().getWorkbook().createFont();
+            font.setFontHeight((short) singleCellFontSize);
+            CellUtil.setFont(cell, font);
+        }
+    }
+}
