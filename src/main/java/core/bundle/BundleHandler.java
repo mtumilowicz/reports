111b8e10fc9c6cda8a98e1679fb2bf3d02d21300
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-08-19 21:05:28 +0200

    removing locale

--- src/main/java/core/bundle/BundleHandler.java
+++ src/main/java/core/bundle/BundleHandler.java
@@ -3,7 +3,6 @@ package core.bundle;
 import com.google.common.base.Preconditions;
 import core.constants.CoreConstants;
 
-import java.util.Locale;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
@@ -15,7 +14,7 @@ public final class BundleHandler {
     private final ResourceBundle bundle;
 
     public BundleHandler() {
-        bundle = ResourceBundle.getBundle(CoreConstants.GENERAL_RESOURCES, new Locale("pl", "PL"));
+        bundle = ResourceBundle.getBundle(CoreConstants.GENERAL_RESOURCES);
     }
 
     public BundleHandler(Class clazz) {
